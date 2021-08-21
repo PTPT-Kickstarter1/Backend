1f56ec2be029ac2f17b551715f95ce3a2e2fa6ca
Cgcollins91 <73444425+Cgcollins91@users.noreply.github.com>
2021-08-20 21:15:18 -0400

    Add files via upload

---
+++ keys.py
@@ -0,0 +1,11 @@
+photo_keys = ['key', 'full', 'ed', 'med', 'little', 'small', 'thumb', '1024x576', '1536x864']
+
+creator_keys= ['id', 'name', 'slug', 'is_registered', 'is_email_verified', 
+'chosen_currency', 'is_superbacker', 'avatar', 'urls']
+
+location_keys = ['id', 'name', 'slug', 'short_name', 'displayable_name', 'localized_name', 'country', 'state', 'type', 'is_root', 'expanded_country', 'urls']
+
+profile_keys = ['id', 'project_id', 'state', 'state_changed_at', 'name', 'blurb', 'background_color', 'text_color', 'link_background_color', 'link_text_color', 'link_text', 'link_url', 'show_feature_image', 'background_image_opacity', 'should_show_feature_image_section', 'feature_image_attributes']
+
+
+category_keys = ["id", "name", "analytics_name", "slug", "position", "parent_id", "parent_name", "color", "urls"]
