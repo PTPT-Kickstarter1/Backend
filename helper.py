15883c6cc6f2a328b988979d5bbe043ea8a8279d
Cgcollins91 <73444425+Cgcollins91@users.noreply.github.com>
2021-08-24 01:01:23 -0400

    Documented MVP model, start of nearest neighbors

--- helper.py
+++ helper.py
@@ -16,53 +16,123 @@ import re
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+def blurb_and_gram_count(X_train_in, X_test_in, tfidf_def=None, SVD_def=None, include_text=True):
+    """ 
+    Pre-process and Create Document Term Matrix for "blurb" feature via pipe
+    
+    Args:
+        X_train_in (DataFrame): X Training Set in, after plot_and_add_hi_freq feature is run
+        X_test_in (DataFrame: X Test Set in, after plot_and_add_hi_freq feature is run
+        tfidf_def (Dict, optional): Input Args to Initialize TfidfVectorizer. Defaults to None.
+        SVD_def (Dict, optional): nput Args to Initialize SVD. Defaults to None.
+        include_text (bool, optional): . Defaults to True.
+
+    Returns:
+        dtm_train (DataFrame), dtm_test(DataFrame) : Document Term Matrix
+    """
+
+    # Copy input dataframes in and get lengths:
+    X_train, X_test = X_train_in.copy(), X_test_in.copy()
+    n1, n2          = len(X_train), len(X_test)
+    
+    # Get LSA Pre-Process Pipeline (no classifier added yet):
+    lsa    = get_pipe('combined', tfidf_def, SVD_def)
+    
+    # Combine Train/Test Set (Ensure Pre-Process/Wranglinging Performed Uniformly):
+    X      = pd.concat([X_train, X_test], ignore_index=True)
+    
+    # If True create DTM with lsa.fit_transform on 'blurb' column
+    # If False, Not implemented yet
+    if include_text:
+        dtm = lsa.fit_transform(X['blurb'])
+        dtm = pd.DataFrame(data=dtm, index=X.index)
+        
+    else:
+    # Functionality for non-default case not implemented yet
+        return NotImplementedError()
+        dtm = pd.DataFrame()
+    
+    # One Hot Encode Our Added Feature (gram_count_cols)
+    ohe            = ohe_gram_count_cols(X)
+    
+    # Concat blurb Document Term Matrix and One Hot Encoded gram count cols:
+    dtm            = pd.concat([dtm, ohe], axis=1, ignore_index=True)
+    
+    # Split back into Training / Test Set:
+    dtm_training   = dtm.iloc[0:n1]
+    dtm_test       = dtm.iloc[n1:n1+n2]
+
+    return dtm_training, dtm_test
+
+
 def ohe_gram_count_cols(X_in):
-    X_out   = X_in.copy()
-    columns = X_in.columns
+    """ One Hot Encode gram_count_cols.
+
+    Args:
+        X_in (DataFrame): Combined Training/Test DataFrame w/ added features.
+
+    Returns:
+        ohe [DataFrame]: Combined Training/Test Dataframe After One Hot Encode Transform to gram_count_cols
+    """
+    # Cooy X_in
+    X_out    = X_in.copy()
+    columns  = X_in.columns
     ohe_cols = []
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    # define a mapping of chars to integers
+    
+    # There is probably a better way to do the below.
+    # Category Encoders can't One Hot Encode (int) features, so map integers to characters 
+    
+    # Define Mapping from integer to characters
+    alphabet    = 'abcdefghijklmnopqrstuvwxyz'
     int_to_char = dict((i, c) for i, c in enumerate(alphabet))
+    
+    # Our added columns have prefix "succesful_" or "failed_", select these columns:
     for col in columns:
         if ('successful' in col) or ('failed' in col) :
             ohe_cols.append(col)
 
+    # Select Features to One Hot Encode:
     ohe_df     = X_out[ohe_cols].copy()
+    # Initialize OutputDataFrame
     ohe_df_out = pd.DataFrame(columns=ohe_df.columns)
+    
+    # Remap integers in features to characters:
     for col, data in ohe_df.items():
         for index, value in data.items():
             ohe_df_out.at[index, col] = int_to_char[value]
 
+    # One Hot Encode Remapped Features
     enc = ce.OneHotEncoder(cols=ohe_cols, use_cat_names=False)
     ohe = enc.fit_transform(ohe_df_out)
-
+    
+    # Return just the one hot encoded columns
     return ohe
+
 
 def get_pipe(pipe_type, tfidf_def=None, SVD_def=None):
     """
-    Set-Up Pipe
-    :param pipe_type: lsa, svd, combined
-    :param tfidf_def: **kwargs to initialize TfidfVectorizer
-    :param SVD_def: ** kwargs to initialize TruncatedSVD
-    :return:
+    Set-Up Pipeline with Simple Full Model or Pre-procesing steps for use on subset of input data.
+    
+    :param pipe_type (string): lsa, svd, or combined:
+        lsa      - Pipe with TfidfVectorizer, Truncated SVD, and Logistic Regression Classifier
+        svd      - Pipe with TfidfVectorizer and LinearSVC
+        comvined - Pipe with TfidfVectorizer and Truncated SVD (classifier step is added 
+                        later on since we are running this on a subset of input features)
+    :param tfidf_def (Dict): **kwargs to initialize TfidfVectorizer
+    :param   SVD_def (Dict): ** kwargs to initialize TruncatedSVD
+    
+    :return: pipe_out (sklearn.Pipeline): Selected Pipeline
     """
-    pipe_out = []
-    if tfidf_def is None:
-        tfidf_def = {'stop_words': 'english',
-                     'ngram_range': (1, 2),
-                     'min_df': 2,
-                     'max_df': .5
-                     }
-    if SVD_def is None:
-        SVD_def = {'n_components': 20,
-                   'algorithm': 'randomized',
-                   'n_iter': 100
-                   }
-
-    vect = TfidfVectorizer(**tfidf_def)
-    svm = LinearSVC()
-    svd = TruncatedSVD(**SVD_def)
-    lr = LogisticRegression(solver='lbfgs')
+    pipe_out           = []
+    
+    # Get **kwargs for tfidf/SVD initialization
+    tfidf_def, SVD_def = get_tfidf_svd_def(tfidf_def, SVD_def)
+    vect               = TfidfVectorizer(**tfidf_def)
+    svd                = TruncatedSVD(**SVD_def)
+    
+    svc = LinearSVC()
+    lr  = LogisticRegression(solver='lbfgs')
     lsa = Pipeline([('vect', vect), ('svd', svd)])
 
     if pipe_type == 'lsa':
@@ -71,10 +141,10 @@ def get_pipe(pipe_type, tfidf_def=None, SVD_def=None):
             ('clf', lr)
         ])
 
-    elif pipe_type == 'svd':
+    elif pipe_type == 'svc':
         pipe_out = Pipeline([
             ('vect', vect),
-            ('clf', svm)]
+            ('clf', svc)]
         )
 
     elif pipe_type == 'combined':
@@ -82,10 +152,49 @@ def get_pipe(pipe_type, tfidf_def=None, SVD_def=None):
 
     return pipe_out
 
-def make_feature(X, vect, unique_grams, success, fail, feature):
+
+def get_tfidf_svd_def(tfidf_def, SVD_def):
+    """ Gets Default **kwawargs for Truncated SVD and TfidfVectorizer used in model.
+
+    Args:
+        tfidf_def (Dict): Dict with **kwargs for TfidfVectorizer initialization
+        SVD_def (Dict): Dict with **kwargs for Truncated SVD initialization
+    """
+    if tfidf_def is None:
+        tfidf_def = {'stop_words': 'english',
+                    'ngram_range': (1, 2),
+                    'min_df'     : 2,
+                    'max_df'     : .5
+                    }
+        
+    if SVD_def is None:
+        SVD_def = {'n_components': 20,
+                   'algorithm'   : 'randomized',
+                   'n_iter'      : 100
+                   }
     
+    return tfidf_def, SVD_def
+
+
+def make_feature(X_in, vect, success, fail, feature):
+    """ Make <state>_ngram_counts features. Feature counts occurence of 
+    words/phrases most frequent and unique to each target state
+    (<state>: successful or failed)
+
+    Args:
+        X_in (DataFrame)      : Combined X_train / X_test DataFrame in
+        vect (CountVectorizer): Initialized CountVectorizer
+        success (list)        : List of words/phrases uniquely and most frequently appearing in successful <feature>
+        fail (list)           : List of words/phrases uniquely and most frequently appearing in failed <feature>
+        feature (string))     : Existing Text Feature Column Used to Create New Feature
+
+    Returns:
+        X(DataFrame): Input DataFrame with Added Features
+    """
+    X = X_in.copy()
     feature_txt = X[feature].tolist()
     vect.fit(feature_txt)
+    unique_grams = success + fail
 
     dtm  = vect.transform(feature_txt)
     df   = pd.DataFrame(dtm.todense(), columns=vect.get_feature_names())
@@ -115,15 +224,30 @@ def make_feature(X, vect, unique_grams, success, fail, feature):
     X[col2] = f_list
     return X
 
+
 def get_unique(text_in, feature='blurb'):
-    
-    target_class = ['successful', 'failed']
+    """ Get Most Common words/phrases in successful/failed 
+        campaign for text <feature> that do not appear in the other class.
+        
+    Args:
+        text_in (Dict): Dictionary with most common words/phrases for <feature> associated with each target class
+        feature (str, optional): [description]. Defaults to 'blurb'.
+
+    Returns:
+        success_out (list): Most common words/phrases associated with text <feature> unique to successful target class
+        fail_out    (list): Most common words/phrases associated with text <feature> unique to successful target class
+    """
     success_out, fail_out = [], []
+    target_class = ['successful', 'failed']
+    # Dict Keys for <feature> of word count for each target class
     key1    = target_class[0] + "_ngrams_" + feature
     key2    = target_class[1] + "_ngrams_" + feature
+    
+    # Most common words/phrases in each target class' text <feature>
     success = set(text_in[key1])
     fail    = set(text_in[key2])
     
+    # Get words/phrases unique to each target class
     for item in success:
         if (item not in fail) :
             success_out.append(item)
@@ -132,6 +256,7 @@ def get_unique(text_in, feature='blurb'):
             fail_out.append(item)
 
     return success_out, fail_out
+
 
 def plot_frequency_distribution_of_ngrams(sample_texts,
                                           ngram_range = (1, 2),
@@ -190,63 +315,6 @@ def plot_frequency_distribution_of_ngrams(sample_texts,
     return ngrams, counts
 
 
-def plot_sample_length_distribution(sample_texts):
-    """Plots the sample length distribution.
-
-    # Arguments
-        samples_texts: list, sample texts.
-    """
-    plt.hist([len(s) for s in sample_texts], 50)
-    plt.xlabel('Length of a sample')
-    plt.ylabel('Number of samples')
-    plt.title('Sample length distribution')
-    plt.show()
-    
-    def count(tokens):
-        """
-    Calculates some basic statistics about tokens in our corpus (i.e. corpus means collections text data)
-    """
-    # stores the count of each token
-
-    word_counts = Counter()
-
-    # stores the number of docs that each token appears in
-    appears_in = Counter()
-    total_docs = len(tokens)
-
-    for token in tokens:
-        # stores count of every appearance of a token
-        word_counts.update(token)
-        # use set() in order to not count duplicates, thereby count the num of docs that each token appears in
-        appears_in.update(set(token))
-
-    # build word count dataframe
-    temp = zip(word_counts.keys(), word_counts.values())
-    wc = pd.DataFrame(temp, columns=['word', 'count'])
-
-    # rank the the word counts
-    wc['rank'] = wc['count'].rank(method='first', ascending=False)
-    total = wc['count'].sum()
-
-    # calculate the percent total of each token
-    wc['pct_total'] = wc['count'].apply(lambda token_count: token_count / total)
-
-    # calculate the cumulative percent total of word counts
-    wc = wc.sort_values(by='rank')
-    wc['cul_pct_total'] = wc['pct_total'].cumsum()
-
-    # create dataframe for document stats
-    t2 = zip(appears_in.keys(), appears_in.values())
-    ac = pd.DataFrame(t2, columns=['word', 'appears_in'])
-
-    # merge word count stats with doc stats
-    wc = ac.merge(wc, on='word')
-
-    wc['appears_in_pct'] = wc['appears_in'].apply(lambda x: x / total_docs)
-
-    return wc.sort_values(by='rank')
-
-
 def my_tokenizer(text):
     clean_text = re.sub('[^a-zA-Z ]', '', text)
     tokens = clean_text.lower().split()
@@ -260,13 +328,33 @@ def plot_and_add_hi_freq_feature(X_train_in,
                                  ngram_range=(1,2),
                                  num_ngrams=50,
                                  figsize=(20,20)):
-    
+    """ Plot Frequency Distribution for top <num_ngrams> in <feature> for each class of target
+
+    Args:
+        X_train_in (DataFrame): X Train In
+        y_train_in (DataFrame): y Train In
+        X_test_in (DataFrame): X Test in
+        feature (string): Column Name of feature to be plotted and used to create new feature
+        ngram_range (tuple, optional): range of number of words to be considered for each ngram. Defaults to (1,2).
+        num_ngrams (int, optional): # of ngrams to plot. Defaults to 50.
+        figsize (tuple, optional): Figure Size. Defaults to (20,20).
+
+    Returns:
+        [type]: [description]
+    """
+    # Copy input dataframes
     X_train, y_train, X_test = X_train_in.copy(), y_train_in.copy(), X_test_in.copy()
     target_class             = ['successful', 'failed']
+    
+    # Initialize Dict with unique and most frequent word/phrases for each target class
+    # and their counts
     blurb = {}
     for state in target_class:
+        # Select subset of input dataframe associated with each target class:
         idx          = (y_train == state)
         X_state      = X_train[idx]
+        
+        # Select Text Feature, Conver
         state_text   = X_state[feature].tolist()
         title        = 'Frequency Distribution of ngrams for ' + state
         
@@ -283,8 +371,6 @@ def plot_and_add_hi_freq_feature(X_train_in,
     # Get the unique ngrams with the highest frequency associated with each class and create numerical feature
     # with how often the highest frequency, unique ngram appeared in the description
     success, fail = get_unique(blurb, feature)
-
-
     kwargs = {
         'ngram_range'  : ngram_range,
         'dtype'        : 'int32',
@@ -297,7 +383,7 @@ def plot_and_add_hi_freq_feature(X_train_in,
     unique_grams = success + fail
     n1, n2       = len(X_train), len(X_test_in)
     X_combined   = pd.concat([X_train, X_test],ignore_index=True)
-    X_combined   = make_feature(X_combined, vect, unique_grams, success, fail, feature)
+    X_combined   = make_feature(X_combined, vect, success, fail, feature)
     
     X_train = X_combined.iloc[0:n1]
     X_test  = X_combined.iloc[n1:n1 + n2]
--- model.py
+++ model.py
@@ -18,83 +18,118 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.pipeline import make_pipeline
+from sklearn.neighbors import NearestNeighbors
+
 
 # %%
-def blurb_and_gram_count(X_train_in, X_test_in, tfidf_def=None, SVD_def=None, include_text=True):
-    X_train, X_test = X_train_in.copy(), X_test_in.copy()
-    lsa    = get_pipe('combined', tfidf_def, SVD_def)
-    n1, n2 = len(X_train), len(X_test)
-    X      = pd.concat([X_train, X_test], ignore_index=True)
-    
-    if include_text:
-        dtm = lsa.fit_transform(X['blurb'])
-        dtm = pd.DataFrame(data=dtm, index=X.index)
-    else:
-        dtm = pd.DataFrame()
+# Load Spacy Large:
+nlp       = spacy.load("en_core_web_lg")
 
-    ohe            = ohe_gram_count_cols(X)
-    dtm            = pd.concat([dtm, ohe], axis=1, ignore_index=True)
-    dtm_training   = dtm.iloc[0:n1]
-    dtm_test       = dtm.iloc[n1:n1+n2]
-
-    return dtm_training, dtm_test
-# %%
-# Load Spacy large
-nlp = spacy.load("en_core_web_lg")
-
+# Load training and test data frames in from pickle:
 target    = 'state'
 df_train  = pd.read_pickle('./df_train.pkl')
 df_test   = pd.read_pickle('./df_test.pkl')
 
+#  Seperate Train/Test into X / y :
 X_train, y_train = df_train.drop(columns=[target]), df_train[target]
 X_test, y_test   = df_test.drop(columns=[target]), df_test[target]
 
 # %%
+# Use NLP cols to create new feature counting most occurence of most common but unique to successful/failed campaigns
+# In model, NLP col is TfidfVectorized and then run through SVD
 nlp_cols  = ['blurb']
-cat_cols  = ['country', 'spotlight', 'staff_pick', 'currency']
-num_cols  = ['converted_pledged_amount', 'backers_count', 'goal', 'pledge_pct_goal', 'campaign_length']
 
-# %%
+# Categorical features used in model:
+cat_cols  = ['country', 'spotlight', 'staff_pick', 'currency']
+
+# Numerical features used in model:
+num_cols  = ['backers_count', 'goal', 'campaign_length']
+
+# %% 
+# Select numerical features from training/test set
 X_train_numeric = X_train[num_cols]
 X_test_numeric  = X_test[num_cols]
-X_combined_numeric = pd.concat((X_train_numeric, X_test_numeric), axis=1, ignore_index=True)
 
+# Concat into one matrice so all feature engineering is performed uniformly on train/test sets:
+X_combined_numeric = pd.concat((X_train_numeric, X_test_numeric), ignore_index=True)
+
+# Impute missing numerical features using median and normalize:
 pipe = make_pipeline(
     SimpleImputer(strategy = 'median'),
     Normalizer(norm='l1'),
 )
 
-n1, n2 = len(X_train), len(X_test)
+# Fit Transform on Combined Train/Test Set:
 dtm_train_test_num = pipe.fit_transform(X_combined_numeric)
+
+# Seperate back out into train/test set:
+n1, n2        = len(X_train), len(X_test)
 dtm_train_num = dtm_train_test_num[0:n1]
 dtm_test_num  = dtm_train_test_num[n1:n1+n2]
 
 # %%
+# 1. Get the <num_ngrams> most common <ngram_range> words/ phrases in the description of successful and failed campaigns
+# 2. Plot the frequency distirbution for successful/failed phrases
+# 3. Find phrases unique to successful or failed campaigns 
+# 4. Count number of occurences of most common unique successful and failed phrases in each project's description
+# 5. Create feature of count for successful and failed
+# 6. One Hot Encode Created Feature
 feature , figsize, num_ngrams, ngram_range = 'blurb', (20,20), 50, (1,2)
 
 X_train, X_test = plot_and_add_hi_freq_feature(X_train, y_train, X_test, feature, ngram_range, num_ngrams, figsize)
 # %%
+# Set Parameters for TfidfVectorizer:
 tfidf_def = {
     'max_df': .7,
     'min_df': .2,
     'max_features': 300,
 }
 
+# Set Parameters for Singular Value Decomposition
 SVD_def = {
     'n_components': 5,
     'algorithm': 'randomized',
     'n_iter': 100
     }
 
-# One Hot Encode <state>_ngram_blurb cols and tfidf/SVD blurb 
+# Run 
 dtm_train, dtm_test = blurb_and_gram_count(X_train, X_test, tfidf_def, SVD_def, True)
 # %%
-dtm_train = np.concatenate([dtm_train, dtm_train_num],axis=1)
-dtm_test  = np.concatenate([dtm_test, dtm_test_num],axis=1)
+# Combine dtm from Tdidf/SVD of blurb
+dtm_train = np.concatenate([dtm_train, dtm_train_num], axis=1)
+dtm_test  = np.concatenate([dtm_test, dtm_test_num], axis=1)
+
 # %%
+# Initialize RandomForestClassifier with default setting and fit model on test
 clf                 = RandomForestClassifier()
 clf                 = clf.fit(dtm_train, y_train)
+
 # %%
+# Predict target for test data and calculate accuracy:
 y_pred = clf.predict(dtm_test)
 score  = accuracy_score(y_pred, y_test)
-# %%
+print(score)
+
+# %% INWORK
+from sklearn.neighbors import NearestNeighbors
+nn = NearestNeighbors(n_neighbors=5, algorithm="kd_tree")
+
+# test description:
+ks_description = "An App that helps you select a kickstarter project likely to succeed"
+
+# add ideal description to end of job_listing DataFrame (copy and create new df)
+job_listing_w_ideal = job_listing.copy()
+job_listing_w_ideal.loc[len(job_listing.index)] = [ideal_description, 'Data Scientist']
+
+# vectorize job_listing w/ ideal description added using Tfidf 
+# this step is required if our ideal posting introduces any new vocabulary
+wc_ideal, dtm_ideal = vec(job_listing_w_ideal, 'TfidfVectorizer')
+
+# Fit on DTM
+nn.fit(dtm_ideal)
+
+# Pull our added ideal description to query listing
+doc_vector = [dtm_ideal.iloc[len(dtm_ideal)-1]]
+
+# Query Using kneighbors 
+neigh_dist, neigh_ind = nn.kneighbors(doc_vector)
